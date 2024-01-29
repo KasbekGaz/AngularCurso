de63d6d2d7953d709ade7b389ef39063f4f84295
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-28 19:37:23 -0600

    componente contador

---
+++ src/app/contador/conta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    templateUrl: './conta.component.html',
+    styleUrl: './conta.component.css'
+})
+
+export class ContadorComponent{
+
+}
